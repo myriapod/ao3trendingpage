ae88abb9b2fb16b3171360c19a1319c12a425ca3
myriapod <solene.poydenot@gmail.com>
2024-01-18 18:03:48 +0100

    On branch website Changes to be committed: new file:   website/homepage.css modified:   website/index.html new file:   website/pictures/ao3_logo_42.png modified:   website/ranking.js
    started working on the index html + js + css page
    stopped at the js for a show / show all button

--- website/ranking.js
+++ website/ranking.js
@@ -1,26 +1,68 @@
-const mariadb = require('mariadb');
-const pool = mariadb.createPool({
-     host: 'mydb.com', 
-     user:'root', 
-     password: 'root',
-     connectionLimit: 5
-});
-async function asyncFunction() {
-  let conn;
-  try {
-	conn = await pool.getConnection();
-	const rows = await conn.query("SELECT 1 as val");
-	console.log(rows); //[ {val: 1}, meta: ... ]
-	const res = await conn.query("INSERT INTO myTable value (?, ?)", [1, "mariadb"]);
-	console.log(res); // { affectedRows: 1, insertId: 1, warningStatus: 0 }
+var fandomObject = {
+  "ATEEZ": "ATEEZ"}
+  // list of fandom available to be looked into, currently just Ateez is working
 
-  } catch (err) {
-	throw err;
-  } finally {
-	if (conn) return conn.end();
+window.onload = function() {
+  // Dropdown menu to select which fandom to look into
+  var fandomSel = document.getElementById("fandom-choice");
+  for (var x in fandomObject) {
+    fandomSel.options[fandomSel.options.length] = new Option(x, x);
+  };
+  fandomSel.onchange = function() {
+    // important var for future references //
+    var fandomChosen = fandomObject[this.value];
+    /////////////////////////////////////////
+    document.querySelector("#fandom-chosen").innerHTML = fandomChosen;
+  };
+
+  document.querySelectorAll("#show-button").forEach(elem => elem.addEventListener("click", () => {
+    document.querySelector("#button-check").innerHTML = "<p>button clicked!</p>";
+  }));
+  
+  /* document.querySelector("#button-check").innerHTML = showButtons.length;
+  
+  showButtons.forEach((item) => {
+    item.addEventListener = function(){
+      document.querySelector("#button-check").innerHTML = "<p>button clicked!</p>";
+
+      item.siblings('.entry-info').slideToggle('slow');
+    };
+  })*/
+
+    
   }
-};
 
-function docWrite() {
-    document.getElementById("demo").innerHTML("We're in the ranking file")
-}
+  // Query the top10.json file
+
+
+
+
+
+//const mariadb = require('mariadb');
+//const pool = mariadb.createPool({
+//     host: 'mydb.com', 
+//     user:'root', 
+//     password: 'root',
+//     connectionLimit: 5
+//});
+//async function asyncFunction() {
+//  let conn;
+//  try {
+//	conn = await pool.getConnection();
+//	const rows = await conn.query("SELECT 1 as val");
+//	console.log(rows); //[ {val: 1}, meta: ... ]
+//	const res = await conn.query("INSERT INTO myTable value (?, ?)", [1, "mariadb"]);
+//	console.log(res); // { affectedRows: 1, insertId: 1, warningStatus: 0 }
+//
+//  } catch (err) {
+//	throw err;
+//  } finally {
+//	if (conn) return conn.end();
+//  }
+//};
+//
+//function docWrite() {
+//    document.getElementById("demo").innerHTML("We're in the ranking file")
+//}
+
+
