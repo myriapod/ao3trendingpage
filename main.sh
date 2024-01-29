211f6f640fe7c2746c55b747f806a051bd893403
myriapod <solene.poydenot@gmail.com>
2024-01-29 17:04:43 +0100

    ao3trendingpage systemd service conf nginx conf works on ao3trendingpage www.ao3trendingpage.com ao3trendingpage.com

--- main.sh
+++ main.sh
@@ -18,5 +18,18 @@ sh setup/reload.sh
 # set up the data import as a cron that runs every day at 3 am
 (crontab -l 2>/dev/null; echo "0 3 * * * setup/reload.sh") | crontab -
 
+sudo apt-get install nginx
+
+sudo cp setup/ao3trendingpage.service /etc/systemd/system/ao3trendingpage.service
+sudo systemctl daemon-reload
+sudo systemctl start ao3trendingpage
+sudo systemctl enable ao3trendingpage
+
+sudo cp setup/ao3trendingpage.conf /etc/nginx/sites-available/ao4trendingpage.conf
+sudo ln -s /etc/nginx/sites-available/ao4trendingpage.conf /etc/nginx/sites-enabled
+sudo systemctl restart nginx
+
 # run the flask app (in dev mode)
-env FLASK_APP=flask-website/website.py python3 -m flask run --debug
+# env FLASK_APP=flask-website/website.py python3 -m flask run --debug
+# deploy in prod with gunicorn
+# gunicorn --config flask-website/gunicorn_config.py --chdir flask-website flask-website.website:app
